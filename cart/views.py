a3fc6f393d21dbe77a3e19c1dd8b9a05098f957f
FAenX <kipronofb@gmail.com>
2019-06-12 21:41:30 +0300

    checkout print post request.

--- cart/views.py
+++ cart/views.py
@@ -12,34 +12,35 @@ def product_validate(request):
     '''
     validate items on cart.
     check if order quantities are available in stock
-    '''
-    d = {}
-    qty=''
-    # received data from ajax request
-    rdata = json.loads(request.GET.get('cartItems', None))
-    for da in rdata:
-        name = da['name']
-        count = da['count']
-        product = Product.objects.filter(product_name=name).values().get()[
-            'id']  # filter product id
-        quantity = Quantity.objects.filter(pk=product).values().get()[
-            'quantity']  # filter product available quantity
-        if int(count) <= int(quantity):
-            d[name] = True
-        else:
-            d[name] = False
-            qty = quantity
+    '''    
+    if request.method == 'GET':
+        d = {}
+        qty=''
+        # received data from ajax request
+        rdata = json.loads(request.GET.get('cartItems', None))
+        for da in rdata:
+            name = da['name']
+            count = da['count']
+            product = Product.objects.filter(product_name=name).values().get()[
+                'id']  # filter product id
+            quantity = Quantity.objects.filter(pk=product).values().get()[
+                'quantity']  # filter product available quantity
+            if int(count) <= int(quantity):
+                d[name] = True
+            else:
+                d[name] = False
+                qty = quantity
 
-    for k, v in d.items():
-        if v is True:
-            data = {'is_available': True} #return is_available true if all items pass test
-        else:
-            data = {
-                'error_message': f"There is only {qty} {k} available, please match the quantity"} #return error if one item fail test
-            print(data)
-            break
+        for k, v in d.items():
+            if v is True:
+                data = {'is_available': True} #return is_available true if all items pass test
+            else:
+                data = {
+                    'error_message': f"There is only {qty} {k} available, please match the quantity"} #return error if one item fail test
+                print(data)
+                break
 
-    return JsonResponse(data)
+        return JsonResponse(data)
 
 
 def checkout_done_view(request):
@@ -54,8 +55,10 @@ def checkout(request):
     process checkout
     reduce items available etc later
     '''
-    items = request.GET.get('items', None)
-    print(items)
+    if request.method == 'POST':
+        items = json.loads(request.POST.get('cartItems', None))
+        print(items)
+        data = {'accepted': 'Recieved'}
+        return JsonResponse(data)
 
-    data = {'accepted': 'Recieved'}
-    return JsonResponse(data)
+        
