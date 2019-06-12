02064520c224e698c932294c69e91e3f60edcb41
FAenX <kipronofb@gmail.com>
2019-06-12 14:33:28 +0300

    final commit

--- grocerystore/urls.py
+++ grocerystore/urls.py
@@ -18,9 +18,11 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from store.views import ProductsListView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     #store app urls
-    path('store/', include('store.urls')),
     path('cart/', include('cart.urls')),
+    path('', ProductsListView.as_view(), name='product_list'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- store/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-#app views
-from .views import ProductsListView
-
-#store app urls
-urlpatterns = [
-    path('index/', ProductsListView.as_view(), name='product_list'),
-]
