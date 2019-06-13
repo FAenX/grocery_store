5bc8421b70639ee75823fa5fdc1873bb41f58f9e
FAenX <kipronofb@gmail.com>
2019-06-13 16:05:14 +0300

    fixed bug that only reduces 1 item

--- cart/views.py
+++ cart/views.py
@@ -14,30 +14,31 @@ def product_validate(request):
     check if order quantities are available in stock
     '''    
     if request.method == 'GET':
+        items = json.loads(request.GET.get('cartItems', None))
         d = {}
-        qty=''
-        # received data from ajax request
-        rdata = json.loads(request.GET.get('cartItems', None))
-        for da in rdata:
-            name = da['name']
-            count = da['count']
-            product = Product.objects.filter(product_name=name).values().get()[
-                'id']  # filter product id
-            quantity = Quantity.objects.filter(pk=product).values().get()[
-                'quantity']  # filter product available quantity
-            if int(count) <= int(quantity):
-                d[name] = True
+        i = len(items)-1
+        
+        
+        while i >= 0:
+            item = items[i]
+            product = Product.objects.filter(product_name=item['name']).values().get()['id']
+            available_quantity = Quantity.objects.filter(pk=product).values().get()['quantity']
+            print(available_quantity)
+            print(item['count'])
+            if available_quantity >= int(item['count']):
+                d[item['name']] = True
             else:
-                d[name] = False
-                qty = quantity
-
+                d[item['name']] = False
+                qty = available_quantity
+            
+            i-=1
+        
         for k, v in d.items():
             if v is True:
                 data = {'is_available': True} #return is_available true if all items pass test
             else:
                 data = {
                     'error_message': f"There is only {qty} {k} available, please match the quantity"} #return error if one item fail test
-                print(data)
                 break
 
         return JsonResponse(data)
@@ -57,10 +58,13 @@ def checkout(request):
     '''
     if request.method == 'POST':
         items = json.loads(request.POST.get('cartItems', None))
-        i = len(items)
-        while i !=0:
-            item = items[0]
+        i = len(items)-1
+        print(i)
+        
+        while i >= 0:
+            item = items[i]
             product = Product.objects.filter(product_name=item['name']).values().get()['id']
+            print(product)
             available_quantity = Quantity.objects.filter(pk=product).values().get()['quantity']
             new_qty = available_quantity - int(item['count'])
             qty_obj = Quantity.objects.get(pk=product)
